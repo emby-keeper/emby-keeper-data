031e156e80ea3ceb7578e1cbcc92f93cd9d18b16
jackzzs <jackzzs@outlook.com>
2023-11-11 21:28:00 +0800

    Update pornemby word list

--- src/gen_map.py
+++ src/gen_map.py
@@ -1,14 +1,14 @@
 from pathlib import Path
 
-patterns = ['{spec}', '{spec}@latest']
+patterns = ["{spec}", "{spec}@latest"]
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     specs = {}
     basedir = Path(__file__).parent.parent
-    data = basedir / 'data'
+    data = basedir / "data"
     for f in data.iterdir():
         try:
-            spec, version = f.stem.split('@')
+            spec, version = f.stem.split("@")
         except ValueError:
             continue
         spec_ext = (spec, f.suffix)
@@ -16,10 +16,10 @@ if __name__ == '__main__':
             specs[spec_ext].append(version)
         else:
             specs[spec_ext] = [version]
-    versionm = basedir / 'version'
-    with open(versionm, 'w+') as f:
+    versionm = basedir / "version"
+    with open(versionm, "w+") as f:
         for (spec, ext), vs in specs.items():
             latest_version = sorted(vs, reverse=True)[0]
             for p in patterns:
                 pattern = p.format(spec=spec)
-                f.write(f'{pattern}{ext} = {spec}@{latest_version}{ext}\n')
+                f.write(f"{pattern}{ext} = {spec}@{latest_version}{ext}\n")
